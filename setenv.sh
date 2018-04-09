9bf34eee689677f4052b4c9f9c49aaa755070b81
Jun Shao <mr.shaojun@gmail.com>
2018-04-09 22:37:40 +1000

    Use shared database instance in ElephantSQL

--- setenv.sh
+++ setenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DATABASE_URI=postgresql://localhost/airmnb
+export DATABASE_URI=postgres://qstqzkzu:wIRQ-yASKMaE7hEdABZCD7cSKUuC40DA@stampy.db.elephantsql.com:5432/qstqzkzu
 
 # load aliases for convenience
 FILE_DIR=$(dirname "${BASH_SOURCE}")
