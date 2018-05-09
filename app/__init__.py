6d45f116b00113c188ae191bddaaa220f0945f15
Jun Shao <mr.shaojun@gmail.com>
2018-05-09 22:51:41 +1000

    Add Access-Control-Allow-XYZ response headers to all responses

--- app/__init__.py
+++ app/__init__.py
@@ -164,6 +164,13 @@ def create_app(config_name):
 				401, {'Content-Type': 'application/json'})
 		return redirect(location='/#/login')
 
+	@app.after_request
+	def apply_cors_headers(response):
+		response.headers['Access-Control-Allow-Origin'] = '*'
+		response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
+		response.headers['Access-Control-Allow-Headers'] = 'Authorization,X-Requested-With'
+		return response
+
 	@app.route('/sys/authorization_response')
 	def authorization_response():
 		state_literal = request.args.get('state', '')
