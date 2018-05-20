8ef0f33866832370db22fce28d3fba36fd30c7c0
Jun Shao <mr.shaojun@gmail.com>
2018-05-20 10:09:01 +1000

    Simplify activity_images table

--- db/schema/activity_images.py
+++ db/schema/activity_images.py
@@ -2,9 +2,8 @@
 from .. import pg, sa, metadata
 
 t_activity_images = sa.Table('activity_images', metadata,
-	sa.Column('activity_image_id', pg.UUID, primary_key=True, autoincrement=False, server_default=sa.text('uuid_generate_v4()'), key=u'activityImageId', doc=''),
+	sa.Column('activity_image_id', pg.UUID, primary_key=True, autoincrement=False, key=u'activityImageId', doc=''),
 	sa.Column('activity_id', pg.UUID, nullable=False, key=u'activityId', doc=''),
-	sa.Column('image_id', pg.UUID, nullable=False, key=u'imageId', doc=''),
+	sa.ForeignKeyConstraint([u'activityImageId'], [u'images.imageId']),
 	sa.ForeignKeyConstraint([u'activityId'], [u'activities.activityId']),
-	sa.ForeignKeyConstraint([u'imageId'], [u'images.imageId']),
 )
