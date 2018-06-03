2c7f97c3796742bc8acf92624098c0d62fd7ffe0
Jun Shao <mr.shaojun@gmail.com>
2018-06-03 10:46:16 +1000

    Add /api/1.0/stat API for statistic info

---
+++ app/api/v1_0/stat.py
@@ -0,0 +1,33 @@
+
+from flask import request, session, jsonify
+
+import db.model as m
+from db.db import SS
+from app.api import api, caps, MyForm, Field, validators
+from app.i18n import get_text as _
+from . import api_1_0 as bp, InvalidUsage
+
+from . import _helper as helper
+
+_name = '/' + __file__.split('/')[-1].split('.')[0]
+
+
+@bp.route(_name, methods=['GET'])
+@api
+@caps()
+def get_stat():
+	userCount = m.User.query.count()
+	babyCount = m.Baby.query.count()
+	providerCount = m.Provider.query.count()
+	activityCount = m.Activity.query.count()
+	bookingCount = m.Vacancy.query.count()
+	transactionCount = bookingCount
+	
+	return jsonify(stat=dict(
+		userCount=userCount, 
+		babyCount=babyCount,
+		providerCount=providerCount,
+		activityCount=activityCount,
+		bookingCount=bookingCount,
+		transactionCount=transactionCount,
+	))
